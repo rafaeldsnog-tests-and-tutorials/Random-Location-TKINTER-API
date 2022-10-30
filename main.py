2eb2a8e07254cc4dbe684f8e0607986d6820ac08
Rafael dos Santos <rafaeldsnog1@gmail.com>
2022-10-30 17:55:01 -0300

    updates 30_10_2022

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def get_location():
     r = ET.fromstring(response.content)
     latitude = float(r[0][0].text)
     longitude = float(r[0][1].text)
-    location = f"{r[0][4].text}, {r[0][8].text}"
+
+    location = f"{r[0][4].text} \nCoordinates: {round(latitude,4)}; {round(longitude,4)}"
 
     loc_label.config(text=location)
     map_widget.set_position(latitude,longitude,marker=True)
@@ -19,8 +20,9 @@ def get_location():
 BG= '#e9ecef'
 root = Tk()
 root.title('RANDOM LOCATION APP')
-root.geometry("600x650")
+root.geometry("600x670")
 root.config(padx=5,pady=5,bg=BG)
+root.resizable(0,0)
 
 main_label = Label(text="MAP", font=("Arial", 20, "bold"),bg=BG)
 main_label.pack(pady=5)
@@ -31,7 +33,7 @@ my_label.pack()
 map_widget = tkintermapview.TkinterMapView(my_label, width=500, height=500, corner_radius=5)
 map_widget.pack()
 
-loc_label = Label(text="Las Vegas, NV", bg=BG, font=("Arial", 11, "bold"))
+loc_label = Label(text="Las Vegas\n Coordinates: 36.1699; -115.1396", bg=BG, font=("Arial", 11, "bold"))
 loc_label.pack(pady=3)
 
 button = Button(text="GET RANDOM LOCATION", font=("Arial", 12, "bold"), command=get_location)
@@ -40,10 +42,6 @@ button.pack(pady=10)
 # SET COORDINATES
 map_widget.set_position(36.1699,-115.1396)
 map_widget.set_zoom(10)
-
-
-
-
 
 root.mainloop()
 
